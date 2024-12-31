33b145ad9eef9ff6f956295d5c0f56fb5b7669db
Rehan khan <143535840+Rehankhan008@users.noreply.github.com>
2024-12-31 18:12:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "aws_instance" "web_servers" {
               sudo apt update -y
               sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
               curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-              sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+              sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
               apt-cache policy docker-ce
               sudo apt install -y docker-ce
               systemctl start docker
