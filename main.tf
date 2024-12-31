9dcbe1fe0efca04ce9f6bdb121308f6b8d5acd3b
Rehan khan <143535840+Rehankhan008@users.noreply.github.com>
2024-12-31 18:18:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -49,16 +49,15 @@ resource "aws_instance" "web_servers" {
   # Install Docker on the instance
   user_data = <<-EOF
               #!/bin/bash
-              sudo apt update -y
-              sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+              apt-get update -y
+              apt-get install -y apt-transport-https ca-certificates curl software-properties-common
               curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-              sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
-              apt-cache policy docker-ce
-              sudo apt install -y docker-ce
-              systemctl start docker
+              add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+              apt-get update -y
+              apt-get install -y docker-ce
               systemctl enable docker
-              usermod -aG docker ubuntu  # Correct username for Ubuntu AMIs
-            EOF
+              systemctl start docker
+              EOF
 
   tags = {
     Name = "WebServer-${count.index + 1}"
