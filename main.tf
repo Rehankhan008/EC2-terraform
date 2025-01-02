55b02b5d0936ba5e2253710212b4adc22fe3ce63
Rehan khan <143535840+Rehankhan008@users.noreply.github.com>
2025-01-02 14:15:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -50,22 +50,21 @@ resource "aws_instance" "web_servers" {
   user_data = <<-EOF
               #!/bin/bash
               # Update and install dependencies
-              apt-get update -y
-              apt-get install apt-transport-https ca-certificates curl software-properties-common
+              sudo apt-get update -y
+              
+              sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
 
               # Add Docker GPG key
               curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
               # Add Docker APT repository
-              add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
+              sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
+              sudo apt-cache policy docker-ce 
               # Update package lists and install Docker
               apt-get update -y
-              apt-get install -y docker-ce
-
-              # Enable Docker to start on boot
-              systemctl enable docker
-              systemctl start docker
+              sudo apt install docker-ce -y
+              sudo systemctl status docker
+              sudo usermod -aG docker ubuntu
               EOF
 
   tags = {
